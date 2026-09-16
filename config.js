3f053ee67ccc67e0c01ce85498e64804b447e07b
liakh10 <119670997+liakh10@users.noreply.github.com>
2026-09-17 02:07:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* ================= CONFIG: единственное место, где меняются данные ================= */
 window.SITE = {
-  CA:      "SOON",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
+  CA:      "0x80503fdbEE0b62EA4335bA111e22De7fe6B8eac9",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
   X_URL:   "https://x.com/stocktipsfun_rh",
   NAME:    "Stock Tips",
   TICKER:  "$STIPS",
